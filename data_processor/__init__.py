b995bdf07fee0c6c4619c086d8bcc0b539e283d2
trow126 <trow126@gmail.com>
2025-04-26 13:47:45 +0900

    特徴量追加

--- data_processor/__init__.py
+++ data_processor/__init__.py
@@ -1,4 +1,10 @@
 # data_processor/__init__.py
 from .feature_engineering import FeatureGenerator, generate_features
+from .feature_engineering_optimized import OptimizedFeatureGenerator, generate_optimized_features
 
-__all__ = ['FeatureGenerator', 'generate_features']
+__all__ = [
+    # 'FeatureGenerator',
+    'generate_features',
+    'OptimizedFeatureGenerator',
+    'generate_optimized_features'
+]
